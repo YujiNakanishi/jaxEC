343f4f3de2edc8adccc3c0e3136a38f1765cccde
nakanishi <nakanishi@astom.co.jp>
2024-02-01 15:17:55 +0900

    add split

--- util.py
+++ util.py
@@ -9,3 +9,22 @@ def sort(solution, fitness):
     """
     sorted_idx = jnp.argsort(fitness)
     return solution[sorted_idx]
+
+
+def split(key, solution, num_pare):
+    """
+    split solution
+
+    input:
+        key -> PRNG key
+        solution -> <jnp:num:(N, D)>
+        num_pare -> <int>
+    
+    output:
+        <tuple:(num_pare, )>
+            <jnp:num:(N/num_pare, D)>
+    """
+    n = int(len(solution)/num_pare)
+    solution = jax.random.permutation(key, solution)
+
+    return (solution[i*n:(i+1)*n] for i in range(num_pare))
