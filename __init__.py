9a6d04d0618cd7962909735682169ea87bf0db95
nakanishi <nakanishi@astom.co.jp>
2024-02-01 10:25:27 +0900

    revise soluation class
    definition of individuals changed (from list to jnp array)
    add __call__ method

--- __init__.py
+++ __init__.py
@@ -7,13 +7,17 @@ class Solution:
     """
     class for individuals management
     att:
-        individuals -> <list:jnp> lift of individual
-                        <jnp:num:(D, )> representation (D is dim)
+        individuals -> <jnp:num:(N, D)> lift of individual
+                        N -> <int> number of individual
+                        D -> <int> dim of representation
         generation -> <int> num of generation
     """
     def __init__(self, individuals, generation = 1):
         self.individuals = copy.deepcopy(individuals)
         self.generation = generation
+    
+    def __call__(self):
+        return self.individuals
     
     def __len__(self):
         return len(self.individuals)
@@ -22,7 +26,7 @@ class Solution:
         return len(self.individuals[0])
     
     def shape(self):
-        return (len(self), self.dim())
+        return self.individuals.shape
     
     def __getitem__(self, idx):
         return self.individuals[idx]
