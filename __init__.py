d8a598be564c5e683e2985da5438ccbf87abbc8c
nakanishi <nakanishi@astom.co.jp>
2024-02-01 11:03:40 +0900

    delete all
    try to make simple, because solution class looks meaningless

--- __init__.py
+++ __init__.py
@@ -2,31 +2,3 @@ import jax.numpy as jnp
 import copy
 
 from jaxEC import selection
-
-class Solution:
-    """
-    class for individuals management
-    att:
-        individuals -> <jnp:num:(N, D)> lift of individual
-                        N -> <int> number of individual
-                        D -> <int> dim of representation
-        generation -> <int> num of generation
-    """
-    def __init__(self, individuals, generation = 1):
-        self.individuals = copy.deepcopy(individuals)
-        self.generation = generation
-    
-    def __call__(self):
-        return self.individuals
-    
-    def __len__(self):
-        return len(self.individuals)
-    
-    def dim(self):
-        return len(self.individuals[0])
-    
-    def shape(self):
-        return self.individuals.shape
-    
-    def __getitem__(self, idx):
-        return self.individuals[idx]
