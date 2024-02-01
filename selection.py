f1bb27137f35045c0030b5f702be32c12f1f33f4
nakanishi <nakanishi@astom.co.jp>
2024-02-01 11:29:25 +0900

    add FPS function

--- selection.py
+++ selection.py
@@ -1,2 +1,44 @@
 import jax.numpy as jnp
 from jax import jit
+import jax
+from functools import partial
+
+@partial(jit, static_argnames = ["selection_num"])
+def greatest(solution, fitness, selection_num):
+    """
+    output solution which individuals are selection_num of greatest one with respect to fitness
+
+    input:
+        solution -> <jnp:num:(N, D)> list of individual
+                    N -> <int> number of individual
+                    D -> <int> dim of individual
+        fitness -> <jnp:num:(N, )> fitness value of each individuals
+        selection_num -> <int> number of selected individuals
+        key -> a PRNG key
+    output:
+        survivor -> <jnp:num:(selection_num, D)> greatest solution
+    """
+    index = jnp.argsort(fitness)
+    survivor = solution[index[:selection_num]]
+
+    return survivor
+
+@partial(jit, static_argnames = ["selection_num"])
+def FPS(key, solution, fitness, selection_num):
+    """
+    output solution by FPS
+
+    input:
+        solution -> <jnp:num:(N, D)> list of individual
+                    N -> <int> number of individual
+                    D -> <int> dim of individual
+        fitness -> <jnp:num:(N, )> fitness value of each individuals
+        selection_num -> <int> number of selected individuals
+        key -> a PRNG key
+    output:
+        survivor -> <jnp:num:(selection_num, D)> greatest solution
+    """
+    probability = fitness/jnp.sum(fitness)
+    survivor = jax.random.choice(key, solution, (selection_num,), p = probability, axis = 0)
+
+    return survivor
